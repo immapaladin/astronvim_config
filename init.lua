a0387d943dc498168ec83d08c0ebda606ebb5bd1
immapaladin <87251242+immapaladin@users.noreply.github.com>
2023-01-26 18:27:46 -0600

    add powershell as toggle term

--- init.lua
+++ init.lua
@@ -357,6 +357,14 @@ local config = {
     --     ["~/%.config/foo/.*"] = "fooscript",
     --   },
     -- }
+    --
+    vim.opt.shortmess:append { s = true, I = true } -- disable startup message
+    vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+    vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;"
+    vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+    vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+    vim.opt.shellquote = ""
+    vim.opt.shellxquote = ""
   end,
 }
 
